76bd3a678030599dd10fb8c165e0c66286041dea
Samuel Herrera Jaraba <futbol.ingeniero@gmail.com>
2024-08-10 15:19:07 -0400

    Segundo ejercicio Anagrama

---
+++ Anagrama.py
@@ -0,0 +1,22 @@
+""" 
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+"""
+
+def anagrama(palabra1, palabra2):
+    palabra1 = list(palabra1.lower())
+    palabra2 = list(palabra2.lower())
+
+    if palabra1 == palabra2:
+        return False
+    
+    return sorted(palabra1) == sorted(palabra2)
+    
+
+
+resultado = anagrama("daewaa", "aewdaa")
+print(resultado)
